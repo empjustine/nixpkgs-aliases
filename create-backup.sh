8622272d9f267c52f5cf5501e45fc6e32bdffe0a
Emp. Justine <1224205+empjustine@users.noreply.github.com>
2023-10-05 21:24:12 -0300

    upkeep

--- create-backup.sh
+++ create-backup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+set -ex
+
+sqlite3 ':memory:' 'SELECT sqlite_version();'
+
 sqlite3 database.sqlite3 .dump >database.sql
--- restore-backup.sh
+++ restore-backup.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+set -ex
+
+sqlite3 ':memory:' 'SELECT sqlite_version();'
+
 rm database.sqlite3
 
 cat database.sql | grep -Ev '^INSERT INTO' >database-sorted.sql
 (
-  printf 'PRAGMA foreign_keys=OFF;\nBEGIN TRANSACTION;\n'
-  cat database.sql | grep -E '^INSERT INTO' | sort -u
-  printf 'COMMIT;\n'
+	printf 'PRAGMA foreign_keys=OFF;\nBEGIN TRANSACTION;\n'
+	cat database.sql | grep -E '^INSERT INTO' | sort -u
+	printf 'COMMIT;\n'
 ) >>database-sorted.sql
 
 cat database-sorted.sql | sqlite3 database.sqlite3
