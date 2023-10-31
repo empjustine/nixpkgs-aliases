4fa0fb8c6056bb34c29049db9bb539bb385bff30
Emp. Justine <1224205+empjustine@users.noreply.github.com>
2023-10-31 14:50:38 -0300

    upkeep

--- create-backup.sh
+++ create-backup.sh
@@ -5,10 +5,11 @@ set -ex
 sqlite3 ':memory:' 'SELECT sqlite_version();'
 
 # sqlite3 database.sqlite3 .dump >database.sql
-
-printf '%s\n' 'PRAGMA foreign_keys=0;' >database.sql
-printf '%s\n' 'PRAGMA ignore_check_constraints=1;' >>database.sql
-printf '%s\n' 'BEGIN TRANSACTION;' >>database.sql
-sqlite3 database.sqlite3 '.schema --indent' >>database.sql
-sqlite3 database.sqlite3 '.dump --data-only' | sort -u >>database.sql
-printf '%s\n' 'COMMIT;' >>database.sql
+{
+	printf '%s\n' 'PRAGMA foreign_keys=0;'
+	printf '%s\n' 'PRAGMA ignore_check_constraints=1;'
+	printf '%s\n' 'BEGIN TRANSACTION;'
+	sqlite3 database.sqlite3 '.schema --indent'
+	sqlite3 database.sqlite3 '.dump --data-only' | sort -u
+	printf '%s\n' 'COMMIT;'
+} >database.sql
--- restore-backup.sh
+++ restore-backup.sh
@@ -6,15 +6,15 @@ sqlite3 ':memory:' 'SELECT sqlite_version();'
 
 rm database.sqlite3
 
-grep <database.sql -Ev '^INSERT INTO' >database-sorted.sql
 (
 	# @see https://www.sqlite.org/pragma.html#pragma_foreign_keys
 	# @see https://www.sqlite.org/pragma.html#pragma_ignore_check_constraints
+	grep <database.sql -Ev '^INSERT INTO'
 	printf '%s\n' 'PRAGMA foreign_keys=0;'
 	printf '%s\n' 'PRAGMA ignore_check_constraints=1;'
 	printf '%s\n' 'BEGIN TRANSACTION;'
 	cat database.sql | grep -E '^INSERT INTO' | sort -u
 	printf '%s\n' 'COMMIT;'
-) >>database-sorted.sql
+) >database-sorted.sql
 
 cat database-sorted.sql | sqlite3 database.sqlite3
