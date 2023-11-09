fd569a0a5e032483b96569359f8452d1a2637518
Emp. Justine <1224205+empjustine@users.noreply.github.com>
2023-11-09 01:03:41 -0300

    upkeep

--- bin-override/nixpkgs-aliases-add-package.sh
+++ bin-override/nixpkgs-aliases-add-package.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ $# -lt 1 ]; then
-	echo "Usage: $0 <attrpath> [flakeref]" 2>&1
-	echo "default flakeref: github:NixOS/nixpkgs/nixos-23.05" 2>&1
-	exit 1
+	printf '%s\n' "Usage: [flakeref=github:NixOS/nixpkgs/nixos-23.05] $0 [<attrpath> [<attrpath> ...]]" 2>&1
 fi
 
-jq -c -S --null-input \
-	--arg attrpath "$1" \
-	--arg flakeref "${2:-github:NixOS/nixpkgs/nixos-23.05}" \
-	'{"attrpath":($attrpath),"comment":null,"dependency":null,"disabled":null,"flakeref":($flakeref)}' |
-	tee "legacyPackages.x86_64-linux/${1}"
+for _attrpath in "$@"; do
+	jq -c -S --null-input \
+		--arg attrpath "$_attrpath" \
+		--arg flakeref "${flakeref:-github:NixOS/nixpkgs/nixos-23.05}" \
+		'{"attrpath":($attrpath),"comment":null,"dependency":null,"disabled":null,"flakeref":($flakeref)}' |
+		tee "legacyPackages.x86_64-linux/${_attrpath}"
+done
