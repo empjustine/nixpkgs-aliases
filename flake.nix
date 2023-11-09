6b32f7c9f2b7ea0e43f54115b641dedcbf5589d0
Emp. Justine <1224205+empjustine@users.noreply.github.com>
2023-11-09 19:55:33 -0300

    upkeep

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  inputs.i-23-05.url = "github:NixOS/nixpkgs/nixos-23.05";
-  inputs.i-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs = {
     self,
-    i-23-05,
-    i-unstable,
+    nixpkgs-stable,
+    nixpkgs-unstable,
   }: {};
 }
